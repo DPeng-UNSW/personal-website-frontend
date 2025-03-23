3ff1d544f01769c1ca67865ef905e946de130a5b
David Peng <david.peng050301@gmail.com>
2025-03-23 23:20:28 +1100

    testing gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/repository-name/'
+  base: '/personal-website-frontend/',
+  publicDir: "public",
+  build: {
+    rollupOptions: {
+      input: "public/index.html",
+    },
+  },
 });
